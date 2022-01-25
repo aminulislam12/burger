18f3904475427abf3be6c1a4a289d8f139b5bb2c
Aminul Islam <aminulislammd96@gmail.com>
2022-01-25 22:14:02 +0600

    singleComponentUIUP

--- src/Components/Orders/SingleOrder/SingleorderShow.js
+++ src/Components/Orders/SingleOrder/SingleorderShow.js
@@ -2,6 +2,14 @@ import React from "react";
 import "./single.css";
 
 const SingleOrderShow = (props) => {
+  console.log(props);
+  const ingredients = props.order.ingredient.map((item) => {
+    return (
+      <span key={Math.random()} className="ingredients">
+        {item.amount} x {item.type}
+      </span>
+    );
+  });
   return (
     <>
       <div className="col-md-4">
@@ -22,6 +30,13 @@ const SingleOrderShow = (props) => {
             <i className="bi bi-telephone mx-1"></i>
             {props.order.custometinfo.customerPhone}
           </h5>
+          <div className="mt-3 mb-2">
+            <i class="bi bi-basket-fill mx-1"></i>
+            {ingredients}
+          </div>
+          <span className="badge bg-success price">
+            BDT: {props.order.totalPrice} Tk
+          </span>
         </div>
       </div>
     </>
